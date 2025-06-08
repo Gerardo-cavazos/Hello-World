acae7ea1759c6bdf3f27d430b6e51c966dd717b5
Gerardo-cavazos <gercav77@gmail.com>
2025-06-08 13:34:22 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,40 @@
+// This class is named HelloWorld
+public class HelloWorld {
+
+    /*
+     * The main() method is the entry point of any Java program.
+     * When the program runs, execution starts from this method.
+     */
+    public static void main(String[] args) {
+        // Print a basic greeting to the console
+        System.out.println("Hello, World!");
+
+        // Print your name
+        System.out.println("Name: Gerardo Cavazos");
+
+        // Print your favorite food
+        System.out.println("Favorite Food: Pizza");
+
+        // Print the celebrity you would like to meet
+        System.out.println("Would like to meet: Clint Eastwood");
+
+        // Print your least favorite chore
+        System.out.println("Least Favorite Chore: Doing the dishes");
+
+        // Print what you'd rather be doing
+        System.out.println("Rather be: At the beach");
+    }
+}
+
+/*
+ * What does the main() method represent in a Java program?
+ * - The main() method is the starting point of any Java program.
+ * 
+ * Why is System.out.println() used to display output?
+ * - It prints text or data to the console so the user can see the output of the
+ * program.
+ * 
+ * What is the purpose of comments in the program?
+ * - Comments help explain what the code does. They are ignored by the computer
+ * but useful for programmers.
+ */
